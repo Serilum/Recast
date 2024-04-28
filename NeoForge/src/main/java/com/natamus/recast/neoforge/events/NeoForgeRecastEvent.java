2963dbd03673e8441d8b18b0a125bacd924bdb6d
Rick South <rick@natamus.com>
2024-04-28 18:58:36 +0200

    (v3.3) (1.20.5 Neo) Updated to work with NeoForge's TickEvent changes in 20.5.14.

--- NeoForge/src/main/java/com/natamus/recast/neoforge/events/NeoForgeRecastEvent.java
+++ NeoForge/src/main/java/com/natamus/recast/neoforge/events/NeoForgeRecastEvent.java
@@ -5,16 +5,16 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.neoforged.neoforge.event.TickEvent.PlayerTickEvent;
-import net.neoforged.neoforge.event.entity.player.ItemFishedEvent;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.player.ItemFishedEvent;
+import net.neoforged.neoforge.event.tick.PlayerTickEvent;
 
 @EventBusSubscriber
 public class NeoForgeRecastEvent {
 	@SubscribeEvent
 	public static void onPlayerTick(PlayerTickEvent e) {
-		Player player = e.player;
+		Player player = e.getEntity();
 		Level level = player.level();
 		if (level.isClientSide) {
 			return;
