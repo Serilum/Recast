39bdcfdb5686a606ed809bb5cb33d8faf20c25b9
Rick South <rick@natamus.com>
2024-08-09 08:08:29 +0200

    Recast source code for 1.21.1.

---
+++ Common/src/main/java/com/natamus/recast/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.recast.util;
+
+public class Reference {
+	public static final String MOD_ID = "recast";
+	public static final String NAME = "Recast";
+	public static final String VERSION = "3.5";
+	public static final String ACCEPTED_VERSIONS = "[1.21.1]";
+}
