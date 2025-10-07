9f4e8b39e6e3e89764116475581b2d140129bd9a
Rick South <rick@natamus.com>
2025-10-08 00:59:19 +0200

    Recast source code for 1.21.10.

---
+++ Forge/src/main/java/com/natamus/recast/forge/events/ForgeRecastEvent.java
@@ -0,0 +1,36 @@
+package com.natamus.recast.forge.events;
+
+import com.natamus.recast.events.RecastEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.TickEvent.PlayerTickEvent;
+import net.minecraftforge.event.entity.player.ItemFishedEvent;
+import net.minecraftforge.eventbus.api.bus.BusGroup;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
+
+import java.lang.invoke.MethodHandles;
+
+public class ForgeRecastEvent {
+	public static void registerEventsInBus() {
+		BusGroup.DEFAULT.register(MethodHandles.lookup(), ForgeRecastEvent.class);
+	}
+
+	@SubscribeEvent
+	public static void onPlayerTick(PlayerTickEvent.Post e) {
+		Player player = e.player();
+		Level level = player.level();
+		if (level.isClientSide()) {
+			return;
+		}
+
+		RecastEvent.onPlayerTick((ServerLevel)level, (ServerPlayer)player);
+	}
+	
+	@SubscribeEvent
+	public static void onFishingCatch(ItemFishedEvent e) {
+		RecastEvent.onFishingCatch(e.getDrops(), e.getHookEntity());
+	}
+}
