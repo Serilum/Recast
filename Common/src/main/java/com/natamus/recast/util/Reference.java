b4cddb5034d90fb42e6590827528a633576de2d5
Rick South <rick@natamus.com>
2026-06-18 22:30:23 +0200

    Updated Recast to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/recast/util/Reference.java
+++ Common/src/main/java/com/natamus/recast/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "recast";
 	public static final String NAME = "Recast";
 	public static final String VERSION = "3.7";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
